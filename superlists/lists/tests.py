5cf113a5791408fe6370639e5302ecb8c8c5cb2d
heojae <shrldh3576@naver.com>
2020-04-19 14:33:19 +0900

    chapter_04 add

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -15,9 +15,14 @@ class HomePageTest(TestCase):
     def test_home_page_returns_correct_html(self):
         request = HttpRequest()
         response = home_page(request)
+
         self.assertTrue(response.content.startswith(b'<html>'))
-        self.assertIn(b'<title>To-do lists</title>', response.content)
+        self.assertIn(b'<title>To-do lists</title>',response.content)
         self.assertTrue(response.content.endswith(b'</html>'))
+
+        # self.assertTrue(response.content.startswith(b'<html>'))
+        # self.assertIn(b'<title>To-do lists</title>', response.content)
+        # self.assertTrue(response.content.endswith(b'</html>'))
 
 
 
