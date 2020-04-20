73b12a5c617ad8a967c5a839e3a61b9686793a1c
jisoo <cjisoo0117@gmail.com>
2020-04-21 00:00:38 +0900

    pep8 수정

--- jisoo/project/functional_test.py
+++ jisoo/project/functional_test.py
@@ -8,16 +8,17 @@ import unittest
 # browser.get('http://localhost:8000')
 
 # # 웹 페이지 타이틀이 `To-Do`를 표시하고 있다
-# assert 'To-Do' in browser.title, "Browser title was " + browser.title 
+# assert 'To-Do' in browser.title,"Browser title was " + browser.title
 
 # browser.quit()
+
 
 class NewVisitorTest(unittest.TestCase):
 
     def setUp(self):
         self.browser = webdriver.Chrome('./chromedriver')
         self.browser.implicitly_wait(3)
-    
+
     def tearDown(self):
         self.browser.quit()
 
@@ -30,6 +31,7 @@ class NewVisitorTest(unittest.TestCase):
         self.assertIn('To-Do', self.browser.title)
         # 강제로 테스트 실패를 발생시킨다.
         self.fail('Finish the test!')
-    
+
+
 if __name__ == '__main__':
     unittest.main()
