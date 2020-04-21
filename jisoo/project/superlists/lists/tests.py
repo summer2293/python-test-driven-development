d73860d038d215bf7f0cd5dc6e5c13ef0418571e
jisoo <cjisoo0117@gmail.com>
2020-04-21 17:13:42 +0900

    add chapter 3

--- jisoo/project/superlists/lists/tests.py
+++ jisoo/project/superlists/lists/tests.py
@@ -16,5 +16,5 @@ class HomePageTest(TestCase):
         response = home_page(request)
         print(response.content)
         self.assertTrue(response.content.startswith(b'<html>'))
-        self.assertIn(b'<title>To-Do lists</title>'. response.content)
+        self.assertIn(b'<title>To-Do lists</title>', response.content)
         self.assertTrue(response.content.endswith(b'</html>'))
