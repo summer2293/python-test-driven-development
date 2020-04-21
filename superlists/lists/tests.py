3c5951e5731342484234a00500e02882fd18695e
heojae <shrldh3576@naver.com>
2020-04-21 21:04:27 +0900

    chapter_05 add

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -1,28 +1,59 @@
-from django.urls import reverse, resolve
 from django.test import TestCase
-from django.http import HttpRequest
-from .views import home_page
-from django.template.loader import render_to_string
+from .models import Item
 
 
-# Create your tests here.
-
-# 이 부분은 책의 django version이 1.7 이고 지금은 3.0.5 라서 수민님의 코드를 인용하였습니다
-# 2.0 version 이후부터 url 들고오는 코드 부분들이 바뀌어 졌어요
 class HomePageTest(TestCase):
-    def test_root_url_resolves_to_home_page_view(self):
-        found = resolve('/') # 책과 다르게 reverse. 변경되었다.
-        self.assertEqual(found.func, home_page) # homepage 와 맞는지 check
 
-    def test_home_page_returns_correct_html(self):
-        request = HttpRequest()
-        response = home_page(request)
-        expected_html = render_to_string("home.html")  # .decode()는 byte데이터를 파이썬 유니코드 문자열로 변화한다
-        self.assertEqual(response.content.decode(), expected_html)  # 즉 문자열과 문자열을 비교를 하는 것이다
+    def test_uses_home_template(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
 
 
+    def test_displays_all_list_items(self):
+        Item.objects.create(text='itemey 1')
+        Item.objects.create(text='itemey 2')
+
+        response = self.client.get('/')
+
+        self.assertIn('itemey 1', response.content.decode())
+        self.assertIn('itemey 2', response.content.decode())
+
+
+    def test_can_save_a_POST_request(self):
+        self.client.post('/', data={'item_text': '신규 작업 아이템'})
+
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, '신규 작업 아이템')
+
+
+    def test_redirects_after_POST(self):
+        response = self.client.post('/', data={'item_text': '신규 작업 아이템'})
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response['location'], '/')
+
+
+    def test_only_saves_items_when_necessary(self):
+        self.client.get('/')
+        self.assertEqual(Item.objects.count(), 0)
 
 
 
+class ItemModelTest(TestCase):
 
+    def test_saving_and_retrieving_items(self):
+        first_item = Item()
+        first_item.text = "첫 번째 아이템"
+        first_item.save()
 
+        second_item = Item()
+        second_item.text = "두 번째 아이템"
+        second_item.save()
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text, "첫 번째 아이템")
+        self.assertEqual(second_saved_item.text, "두 번째 아이템")
