f5edcef2cb0c0c1c5255a2ae5ba3e775caeff70a
jinwooklim <aiden.limo@outlook.com>
2020-04-27 02:09:55 +0900

    New URL, View, Template for listing output

--- jwlim/superlists/superlists/urls.py
+++ jwlim/superlists/superlists/urls.py
@@ -18,7 +18,6 @@ from django.urls import path, include
 from lists import views
 
 urlpatterns = [
-    # path('admin/', admin.site.urls),
-    # path('', include('lists.views.home_page'), name='home'),
     path('', views.home_page, name='home'),
+    path('lists/the-only-list-in-the-world/', views.view_list, name='view_list'),
 ]
