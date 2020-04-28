55baff52bbe906aebac9044cc802f5bd98126481
soomin <soomtopia@gmail.com>
2020-04-29 01:22:43 +0900

    chap06 - add new.html

--- soomin/tddjango/superlists/lists/urls.py
+++ soomin/tddjango/superlists/lists/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path("", views.home_page, name="home"),
     path("lists/the-only-list-in-the-world/", views.view_list, name="view_list"),
+    path("lists/new", views.new_list, name="new_list"),
 ]
