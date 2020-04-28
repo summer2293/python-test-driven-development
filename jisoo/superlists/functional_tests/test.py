3a6431e14c7901a71140068a56953fbbd6f915b1
jisoo <cjisoo0117@gmail.com>
2020-04-28 20:25:17 +0900

    목록 출력을 위한 신규 URL, view, template

--- jisoo/superlists/functional_tests/test.py
+++ jisoo/superlists/functional_tests/test.py
@@ -1,6 +1,7 @@
 from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+import time
 
 
 class NewVisitorTest(LiveServerTestCase):
@@ -34,9 +35,13 @@ class NewVisitorTest(LiveServerTestCase):
         # 텍스트를 입력한다.
         inputbox.send_keys("공작깃털 사기")
 
-        # 엔터키를 치면 페이지가 갱신되고 작업 목록에 아이템이 추가된다
+        # 엔터키를 누르면 새로운 URL로 바뀐다. 그리고 작업 목록에 추가된다.
         inputbox.send_keys(Keys.ENTER)
+        edith_list_url = self.browser.current_url
+        self.assertRegex(edith_list_url, "/lists/.+")
+        self.check_for_row_in_list_table("1: 공작깃털 사기")
 
+        # 추가 아이템을 입력할 수 있는 텍스트 상자가 아직 존재한다.
         # 텍스트 입력
         inputbox = self.browser.find_element_by_id("id_new_item")
         inputbox.send_keys("공작깃털을 이용해서 그물 만들기")
@@ -45,6 +50,34 @@ class NewVisitorTest(LiveServerTestCase):
 
         self.check_for_row_in_list_table("1: 공작깃털 사기")
         self.check_for_row_in_list_table("2: 공작깃털을 이용해서 그물 만들기")
+
+        # 새로운 사용자인 프란시스가 사이트에 접속한다.
+
+        # 새로운 브라우저 세션을 이용해서 에디스의 정보가 쿠키를 통해 유입되는 것을 방지한다.
+        self.browser.quit()
+        self.browser = webdriver.Chrome("./chromedriver")
+
+        # 프란시스가 홈페이지에 접속한다
+        # 에디스의 리스트는 보이지 않는다
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name("body").text
+        self.assertNotIn("공작깃털 사기", page_text)
+        self.assertNotIn("그물 만들기", page_text)
+
+        # 프란시스가 새로운 작업 아이템을 입력하기 시작한다
+        inputbox = self.browser.find_element_by_id("id_new_item")
+        inputbox.send_keys("우유 사기")
+        inputbox.send_keys(Keys.ENTER)
+
+        # 프란시스가 전용 URL을 취득한다.
+        francis_list_url = self.browser.current_url
+        self.assertRegex(francis_list_url, "/lists/.+")
+        self.assertEqual(francis_list_url, edith_list_url)
+
+        # 에디스가 입력한 흔적이 없다는 것을 다시 확인한다.
+        page_text = self.browser.find_element_by_tag_name("body").text
+        self.assertNotIn("공작깃털 사기", page_text)
+        self.assertIn("우유 사기", page_text)
 
         # 강제로 테스트 실패를 발생시킨다.
         self.fail("Finish the test!")
