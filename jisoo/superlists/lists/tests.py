dbcb5c80fa423ba497f50ad65b3aea27c7930949
jisoo <cjisoo0117@gmail.com>
2020-04-29 00:59:16 +0900

    url 변경

--- jisoo/superlists/lists/tests.py
+++ jisoo/superlists/lists/tests.py
@@ -29,7 +29,8 @@ class HomePageTest(TestCase):
 
 class ListAndItemModelsTest(TestCase):
     def test_uses_list_template(self):
-        response = self.client.get("/lists/the-only-list-in-the-world/")
+        list_ = List.objects.create()
+        response = self.client.get("/lists/%d/" % (list_.id,))
         self.assertTemplateUsed(response, "list.html")
 
     def test_saving_and_retrieving_items(self):
@@ -62,23 +63,34 @@ class ListAndItemModelsTest(TestCase):
 class ListViewTest(TestCase):
     def test_displays_all_list_items(self):
         list_ = List.objects.create()
-        Item.objects.create(text="itemey 1", list=list_)
-        Item.objects.create(text="itemey 2", list=list_)
+        response = self.client.get("/lists/%d/" % (list_.id,))
+        self.assertTemplateUsed(response, "list.html")
 
-        response = self.client.get("/lists/the-only-list-in-the-world/")
+    def test_displays_only_items_for_that_list(self):
+        correct_list = List.objects.create()
+        Item.objects.create(text="itemey 1", list=correct_list)
+        Item.objects.create(text="itemey 2", list=correct_list)
+
+        other_list = List.objects.create()
+        Item.objects.create(text="다른 목록 아이템 1", list=other_list)
+        Item.objects.create(text="다른 목록 아이템 2", list=other_list)
+
+        response = self.client.get("/lists/%d/" % (correct_list.id,))
 
         self.assertContains(response, "itemey 1")
         self.assertContains(response, "itemey 2")
+        self.assertNotContains(response, "다른 목록 아이템 1")
+        self.assertNotContains(response, "다른 목록 아이템 2")
 
 
 class NewListTest(TestCase):
     def test_saving_a_POST_request(self):
-        self.client.post("/list/new", data={"item_text": "신규 작업 아이템"})
-        print(Item.objects.all(), List.objects.all())
+        self.client.post("/lists/new", data={"item_text": "신규 작업 아이템"})
         self.assertEqual(Item.objects.count(), 1)
         new_item = Item.objects.first()
         self.assertEqual(new_item.text, "신규 작업 아이템")
 
     def test_redirects_after_POST(self):
         response = self.client.post("/lists/new", data={"item_text": "신규 작업 아이템"})
-        self.assertRedirects(response, "/lists/the-only-list-in-the-world/")
+        new_list = List.objects.first()
+        self.assertRedirects(response, "/lists/%d/" % (new_list.id,))
