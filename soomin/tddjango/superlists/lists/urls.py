2d6411b1525d65635f797c3e8cf517f0f2e1ec46
soomin <soomtopia@gmail.com>
2020-04-29 00:08:20 +0900

    chap06 - add list.html

--- soomin/tddjango/superlists/lists/urls.py
+++ soomin/tddjango/superlists/lists/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    url("", views.home_page, name="home"),
-    url(r"^lists/the-only-list-in-the-world/$", views.view_list, name="view_list"),
+    path("", views.home_page, name="home"),
+    path("lists/the-only-list-in-the-world/", views.view_list, name="view_list"),
 ]
