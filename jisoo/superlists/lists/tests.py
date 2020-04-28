236a6e7d95d58616f36f3d4395174f164847a669
jisoo <cjisoo0117@gmail.com>
2020-04-28 21:11:05 +0900

    두 개 폼을 위한 URL적용, views와 tests에 있는 코드 이동, 신규 URL

--- jisoo/superlists/lists/tests.py
+++ jisoo/superlists/lists/tests.py
@@ -26,39 +26,6 @@ class HomePageTest(TestCase):
             remove_csrf_tag(response.content.decode()), remove_csrf_tag(expected_html)
         )
 
-    def test_home_page_can_save_a_POST_request(self):
-        request = HttpRequest()
-        request.method = "POST"
-        request.POST["item_text"] = "신규 작업 아이템"
-
-        response = home_page(request)
-
-        self.assertEqual(Item.objects.count(), 1)
-        new_item = Item.objects.first()
-        self.assertEqual(new_item.text, "신규 작업 아이템")
-
-    def test_home_page_redirects_after_POST(self):
-        request = HttpRequest()
-        request.method = "POST"
-        request.POST["item_text"] = "신규 작업 아이템"
-
-        response = home_page(request)
-
-        self.assertEqual(response.status_code, 302)
-        self.assertEqual(response["location"], "/lists/the-only-list-in-the-world/")
-
-    def test_home_page_only_saves_items_when_necessary(self):
-        request = HttpRequest()
-        home_page(request)
-        self.assertEqual(Item.objects.count(), 0)
-
-    def test_home_page_displays_all_list_items(self):
-        Item.objects.create(text="itemey 1")
-        Item.objects.create(text="itemey 2")
-
-        request = HttpRequest()
-        response = home_page(request)
-
 
 class ItemModelTest(TestCase):
     def test_uses_list_template(self):
@@ -92,3 +59,15 @@ class ListViewTest(TestCase):
 
         self.assertContains(response, "itemey 1")
         self.assertContains(response, "itemey 2")
+
+
+class NewListTest(TestCase):
+    def test_saving_a_POST_request(self):
+        self.client.post("/list/new", data={"item_text": "신규 작업 아이템"})
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, "신규 작업 아이템")
+
+    def test_redirects_after_POST(self):
+        response = self.client.post("/lists/new", data={"item_text": "신규 작업 아이템"})
+        self.assertRedirects(response, '/lists/the-only-list-in-the-world/')
