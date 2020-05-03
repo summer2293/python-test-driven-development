ab193e02758f850f010430e1fecc9e8255d11bd9
soomin <soomtopia@gmail.com>
2020-05-03 19:26:57 +0900

    add layout

--- soomin/tddjango/superlists/functional_tests/tests.py
+++ soomin/tddjango/superlists/functional_tests/tests.py
@@ -85,3 +85,15 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertIn("우유 사기", page_text)
 
         # 둘 다 만족하고 잠자리에 든다.
+
+    def test_layout_and_styling(self):
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 728)
+
+        inputbox = self.browser.find_element_by_id("id_new_item")
+        self.assertAlmostEqual(inputbox.location["x"] + inputbox.size["width"] / 2, 512, delta=10)
+
+        inputbox.send_keys("testing\n")
+
+        inputbox = self.browser.find_element_by_id("id_new_item")
+        self.assertAlmostEqual(inputbox.location["x"] + inputbox.size["width"] / 2, 512, delta=10)
