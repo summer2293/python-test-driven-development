5022fefc08811975ce92ce9ec5be83de10a4018c
minsung <rkdalstjd9@naver.com>
2020-05-06 21:53:34 +0900

    타 스터디원 플더 삭제

--- heojae2/superlists/chapter_01.py
+++
@@ -1,8 +0,0 @@
-from selenium import webdriver
-browser = webdriver.Chrome("/home/shrldh3576/바탕화면/Gekodriver/chromedriver")
-browser.get("http://localhost:8000")
-
-assert "Django" in browser.title
-
-
-
--- heojae2/superlists/lists/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-# models.Model 을 상속 해야 이런 .save(), .objects() 같은 함수들을 쓸수 있다
-
-
-class Item(models.Model):
-    text = models.TextField(default='')
