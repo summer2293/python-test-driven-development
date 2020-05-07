6ad99cb2faa878b8f6ac5a5007de6fcb3f62d726
jinwooklim <aiden.limo@outlook.com>
2020-05-07 23:48:10 +0900

    Refactor the list view to handle the new POST item

--- jwlim/superlists/lists/tests/test_views.py
+++ jwlim/superlists/lists/tests/test_views.py
@@ -66,24 +66,28 @@ class ListViewTest(TestCase):
         response = self.client.get(f'/lists/{correct_list.id}/')
         self.assertEqual(response.context['list'], correct_list)
 
-
-class NewItemTest(TestCase):
     def test_can_save_a_POST_request_to_an_existing_list(self):
         other_list = List.objects.create()
         correct_list = List.objects.create()
 
-        self.client.post(f'/lists/{correct_list.id}/add_item', data={'item_text': '기존 목록에 신규 아이템'})
+        self.client.post(
+            f'/lists/{correct_list.id}/',
+            data={'item_text': '기존 목록에 신규 아이템'}
+        )
 
         self.assertEqual(Item.objects.count(), 1)
         new_item = Item.objects.first()
         self.assertEqual(new_item.text, '기존 목록에 신규 아이템')
         self.assertEqual(new_item.list, correct_list)
 
-    def test_redirects_to_list_view(self):
+    def test_POST_redirects_to_list_view(self):
         other_list = List.objects.create()
         correct_list = List.objects.create()
 
-        response = self.client.post(f'/lists/{correct_list.id}/add_item', data={'item_text': '기존 목록에 신규 아이템'})
+        response = self.client.post(
+            f'/lists/{correct_list.id}/',
+            data={'item_text': '기존 목록에 신규 아이템'}
+        )
 
         self.assertRedirects(response, f'/lists/{correct_list.id}/')
 
--- jwlim/superlists/lists/views.py
+++ jwlim/superlists/lists/views.py
@@ -9,6 +9,9 @@ def home_page(request):
 
 def view_list(request, list_id):
     list_ = List.objects.get(id=list_id)
+    if request.method == 'POST':
+        Item.objects.create(text=request.POST['item_text'], list=list_)
+        return redirect(f'/lists/{list_.id}/')
     return render(request, 'list.html', {'list': list_})
 
 
@@ -23,11 +26,5 @@ def new_list(request):
         # error = "아이템이 비어 있습니다"
         error = "빈 아이템을 등록할 수 없습니다"
         return render(request, 'home.html', {"error": error})
-    return redirect(f'/lists/{list_.id}/')
-
-
-def add_item(request, list_id):
-    list_ = List.objects.get(id=list_id)
-    Item.objects.create(text=request.POST['item_text'], list=list_)
     return redirect(f'/lists/{list_.id}/')
 
