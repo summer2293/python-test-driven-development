b777419facc1dee36f48710b47dad304df67ec4a
minsung <rkdalstjd9@naver.com>
2020-05-11 03:00:39 +0900

    단위 테스트 파일 분리

--- minsung/project/lists/tests/test_views.py
+++ minsung/project/lists/tests/test_views.py
@@ -4,7 +4,7 @@ from django.http import HttpRequest
 from django.template.loader import render_to_string
 from lists.views import home_page
 import re
-from .models import Item, List
+from ..models import Item, List
 
 def remove_csrf_tag(text):
     return re.sub(r'<[^>]*csrfmiddlewaretoken[^>]*>', '', text)
@@ -26,34 +26,6 @@ class HomePageTest(TestCase):
             remove_csrf_tag(expected_html)
         )
 
-
-class ListAndItemModelTest(TestCase):
-    def test_saving_and_retrieving_items(self):
-        list_ = List()
-        list_.save()
-
-        first_item = Item()
-        first_item.text = "첫 번째 아이템"
-        first_item.list = list_
-        first_item.save()
-
-        second_item = Item()
-        second_item.text = "두 번째 아이템"
-        second_item.list = list_
-        second_item.save()
-
-        saved_list = List.objects.first()
-        self.assertEqual(saved_list, list_)
-
-        saved_items = Item.objects.all()
-        self.assertEqual(saved_items.count(), 2)
-
-        first_saved_item = saved_items[0]
-        second_saved_item = saved_items[1]
-        self.assertEqual(first_saved_item.text, "첫 번째 아이템")
-        self.assertEqual(first_saved_item.list, list_)
-        self.assertEqual(second_saved_item.text, "두 번째 아이템")
-        self.assertEqual(second_saved_item.list, list_)
 
 class LiveViewTest(TestCase):
 
