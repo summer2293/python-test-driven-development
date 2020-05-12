6a17f4fdc32d85abd1d4f049dd8055a1a5cb35c4
soomin <soomtopia@gmail.com>
2020-05-12 23:12:30 +0900

    chap10 - modify view - new list for model validation

--- soomin/tddjango/superlists/lists/tests/test_views.py
+++ soomin/tddjango/superlists/lists/tests/test_views.py
@@ -75,6 +75,14 @@ class NewListTest(TestCase):
         new_list = List.objects.first()
         self.assertRedirects(response, f"/lists/{new_list.id}/")
 
+    def test_validation_errors_are_sent_back_to_home_page_template(self):
+        pass
+
+    def test_invalid_list_items_arent_saved(self):
+        self.client.post("/lists/new", data={"item_text": ""})
+        self.assertEqual(List.objects.count(), 0)
+        self.assertEqual(Item.objects.count(), 0)
+
 
 class NewItemTest(TestCase):
     def test_can_save_a_POST_request_to_an_existing_list(self):
