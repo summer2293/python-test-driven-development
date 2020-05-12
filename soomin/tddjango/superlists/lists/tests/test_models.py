a430613d632f7dc7ea46c0dca69d8b492843768a
soomin <soomtopia@gmail.com>
2020-05-12 22:56:25 +0900

    chap10- 단위 테스트 두개 파일로 나누기

---
+++ soomin/tddjango/superlists/lists/tests/test_models.py
@@ -0,0 +1,33 @@
+from django.test import TestCase
+from lists.models import Item, List
+
+
+class ListAndItemModelsTest(TestCase):
+    def remove_csrf(self, origin):
+        csrf_regex = r"<input[^>]+csrfmiddlewaretoken[^>]+>"
+        return re.sub(csrf_regex, "", origin)
+
+    def test_saving_and_retrieving_items(self):
+        list_ = List()
+        list_.save()
+
+        first_item = Item()
+        first_item.text = "첫 번째 아이템"
+        first_item.list = list_
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = "두 번째 아이템"
+        second_item.list = list_
+        second_item.save()
+
+        save_items = Item.objects.all()
+        self.assertEqual(save_items.count(), 2)
+
+        first_saved_item = save_items[0]
+        second_saved_item = save_items[1]
+
+        self.assertEqual(first_saved_item.text, "첫 번째 아이템")
+        self.assertEqual(first_saved_item.list, list_)
+        self.assertEqual(second_saved_item.text, "두 번째 아이템")
+        self.assertEqual(second_saved_item.list, list_)
--- soomin/tddjango/superlists/lists/tests/test_views.py
+++ soomin/tddjango/superlists/lists/tests/test_views.py
@@ -27,37 +27,6 @@ class HomePageTest(TestCase):
         self.assertEqual(response_decode, expected_html)
 
 
-class ItemModelTest(TestCase):
-    def remove_csrf(self, origin):
-        csrf_regex = r"<input[^>]+csrfmiddlewaretoken[^>]+>"
-        return re.sub(csrf_regex, "", origin)
-
-    def test_saving_and_retrieving_items(self):
-        list_ = List()
-        list_.save()
-
-        first_item = Item()
-        first_item.text = "첫 번째 아이템"
-        first_item.list = list_
-        first_item.save()
-
-        second_item = Item()
-        second_item.text = "두 번째 아이템"
-        second_item.list = list_
-        second_item.save()
-
-        save_items = Item.objects.all()
-        self.assertEqual(save_items.count(), 2)
-
-        first_saved_item = save_items[0]
-        second_saved_item = save_items[1]
-
-        self.assertEqual(first_saved_item.text, "첫 번째 아이템")
-        self.assertEqual(first_saved_item.list, list_)
-        self.assertEqual(second_saved_item.text, "두 번째 아이템")
-        self.assertEqual(second_saved_item.list, list_)
-
-
 class ListViewTest(TestCase):
     def test_uses_list_template(self):
         list_ = List.objects.create()
